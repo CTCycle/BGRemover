7e6b9bc787b9bae0222092be1e2060627b1d54a9
CTCycle <thomas.virdis89@gmail.com>
2024-09-05 12:41:16 +0200

    switched install to pyproject.toml

--- scripts/package_setup.bat
+++ scripts/package_setup.bat
@@ -1,7 +1,7 @@
 @echo off
 rem install packages in editable mode
 
-call conda activate BGRemover && cd .. && pip install -e .
+call conda activate BGRemover && cd .. && pip install -e . --use-pep517
 if errorlevel 1 (
     echo Failed to install the package in editable mode
     goto :eof
